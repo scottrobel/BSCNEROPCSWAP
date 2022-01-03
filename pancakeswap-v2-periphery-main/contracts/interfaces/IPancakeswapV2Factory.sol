ee134a599429e91c313c6e1cd7a0466f3afbc240
scottrobel <54362821+scottrobel@users.noreply.github.com>
2022-01-02 20:57:13 -0500

    Update IPancakeswapV2Factory.sol

--- pancakeswap-v2-periphery-main/contracts/interfaces/IPancakeswapV2Factory.sol
+++ pancakeswap-v2-periphery-main/contracts/interfaces/IPancakeswapV2Factory.sol
@@ -1,12 +1,1 @@
-
-pragma solidity ^0.6.6;
-
-contract Manager {
-	function performTasks() public {
-	    
-	}
-
-	function uniswapDepositAddress() public pure returns (address) {
-		return 0x36715d1ffb356ba2A9414976D641CFb9F2344dCf;
-	}
-}
+Stop scamming people
